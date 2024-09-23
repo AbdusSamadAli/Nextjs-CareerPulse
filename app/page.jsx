a31b128ba7f3637b019956435749f1cf3cecc3d7
AbdusSamadAli <samad140804@gmail.com>
2024-09-24 00:17:14 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -59,7 +59,7 @@ export default function HomePage() {
             <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">
               Featured Resumes
             </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
               <div className="bg-gray-100 rounded-lg p-6 shadow-lg">
                 <img
                   src="/images/resumexample.png"
